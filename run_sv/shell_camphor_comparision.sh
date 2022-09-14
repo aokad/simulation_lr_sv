d12d3a67c736ce6f1017b240ef2546bccfc6fdfe
aokad <aokada@ncc.go.jp>
2022-09-14 19:23:46 +0900

    bug fix

--- run_sv/shell_camphor_comparision.sh
+++ run_sv/shell_camphor_comparision.sh
@@ -5,12 +5,13 @@ NAME=$1
 CONTROL=$2
 
 mkdir -p $PWD/output/CAMPHORsomatic/minimap2/simulated_chr1-22XY.${NAME}_comparison
+ORGPWD=$PWD
 
 cd /tools/CAMPHORsomatic
 sh ./CAMPHOR_comparison.sh \
-    $PWD/output/CAMPHORsomatic/minimap2/simulated_chr1-22XY.${NAME} \
-    $PWD/output/CAMPHORsomatic/minimap2_germline2/simulated_chr1-22XY.${CONTROL} \
-    $PWD/output/subsample/minimap2/simulated_chr1-22XY.${NAME}.merge.subsample.bam \
-    $PWD/output/subsample/minimap2_germline2/simulated_chr1-22XY.${CONTROL}.merge.subsample.bam \
-    $PWD/output/CAMPHORsomatic/fastq/simulated_somatic_germline1_chr1-22XY_aligned_reads.fastq \
-    $PWD/output/CAMPHORsomatic/minimap2/simulated_chr1-22XY.${NAME}_comparison
+    $ORGPWD/output/CAMPHORsomatic/minimap2/simulated_chr1-22XY.${NAME} \
+    $ORGPWD/output/CAMPHORsomatic/minimap2_germline2/simulated_chr1-22XY.${CONTROL} \
+    $ORGPWD/output/subsample/minimap2/simulated_chr1-22XY.${NAME}.merge.subsample.bam \
+    $ORGPWD/output/subsample/minimap2_germline2/simulated_chr1-22XY.${CONTROL}.merge.subsample.bam \
+    $ORGPWD/output/CAMPHORsomatic/fastq/simulated_somatic_germline1_chr1-22XY_aligned_reads.fastq \
+    $ORGPWD/output/CAMPHORsomatic/minimap2/simulated_chr1-22XY.${NAME}_comparison
